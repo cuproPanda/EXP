5c081184ab2ad07963dcf86f07b8d06cdc4b665a
cuproPanda <cupropanda@gmail.com>
2017-05-21 22:46:50 -0400

    Rubber tree description updated, ItemSpawner no longer gives latex

--- Source/ExpandedPower/Items/RubberTreeItemSpawner.cs
+++ Source/ExpandedPower/Items/RubberTreeItemSpawner.cs
@@ -7,7 +7,6 @@ namespace ExpandedPower {
     public override void SpawnSetup(Map map, bool respawningAfterLoad) {
       base.SpawnSetup(map, respawningAfterLoad);
       SpawnRandomQuantity(ThingDef.Named("EXP_RubberWood"), 10, 20);
-      SpawnRandomQuantity(ThingDef.Named("EXP_LatexBucket"), 1, 2);
       Destroy();
     }
   }
